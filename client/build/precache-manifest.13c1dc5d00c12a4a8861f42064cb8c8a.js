68b23ec8416ac503292bec0be9264a57d7711744
yvonneCh <yc.yvonnechen@yahoo.com>
2019-05-24 17:21:17 -0700

    updated and ejected client dependencies

---
+++ client/build/precache-manifest.13c1dc5d00c12a4a8861f42064cb8c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b08966e222f1fbf321b989311d71bbd6",
+    "url": "/static/media/ucla-samueli-logo-white.b08966e2.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7fe20457169ad02e3655",
+    "url": "/static/js/main.7fe20457.chunk.js"
+  },
+  {
+    "revision": "10dd9b8f83522e64cb47",
+    "url": "/static/js/1.10dd9b8f.chunk.js"
+  },
+  {
+    "revision": "7fe20457169ad02e3655",
+    "url": "/static/css/main.9e8320e3.chunk.css"
+  },
+  {
+    "revision": "25bbeb13fa8a9a29dec5bd7dfebca4c5",
+    "url": "/index.html"
+  }
+];
